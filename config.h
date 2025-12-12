059fcbeaa3b6d0a6275b78125caeaa95e2e53b15
renato-osec <renato@osec.io>
2025-12-12 12:29:30 +0100

    fix fonts

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"sourcecodepro:size=14", "fontawesome:size=14"};
+static const char *fonts[]          = {"sourcecodepro:size=14", "fontawesome:size=19"};
 static const char dmenufont[]       = "sourcecodepro:size=12";
 //background color
 static const char col_gray1[]       = "#222222";
