a0a451682f4f92db3c723a92a33e7367e9a76b69
renato-osec <renato@osec.io>
2026-04-14 12:21:38 +0200

    font fix, nix update script

--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sudo nix-update dwm-custom --file /etc/nixos/pkgs/dwm-custom.nix --version branch=main
+# sed neutralizes version so nix-update's same-string guard don't bail
+set -e
+sudo sed -i 's|version = "[^"]*";|version = "0.0.0";|' /etc/nixos/pkgs/dwm-custom.nix
+sudo sh -c 'cd /etc/nixos && nix-update dwm-custom --file /etc/nixos/pkgs/dwm-custom.nix --version branch=main'
 sudo nixos-rebuild switch --impure --flake /etc/nixos#nsa-router
